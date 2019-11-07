46b81ef78178a9620d94734efe19dee866f233b5
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2019-11-07 19:33:33 +0100

    more javascript tests

---
+++ questions.js
@@ -0,0 +1,76 @@
+var Jsondata = [
+    {
+        "title" : "HTML",
+        "questions" : [
+            {  "title" :  "What does HTML stand for?",
+                "type"  :  "select",
+                "option" : 
+                [
+                    { 
+                        "choice" : "option A"
+                        "choice" : "option B"
+                        "choice" : "option C"
+                        "choice" : "option D"
+                    },
+                    {  
+                "title" :  "What does HTML stand for?",
+                "type"  :  "select",
+                "option" : [
+                    { 
+                        "choice" : "option A"
+                        "choice" : "option B"
+                        "choice" : "option C"
+                        "choice" : "option D"
+                    },
+            {  
+                "title" :  "What does HTML stand for?",
+                "type"  :  "select",
+                "option" : [
+                    { 
+                        "choice" : "option A"
+                        "choice" : "option B"
+                        "choice" : "option C"
+                        "choice" : "option D"
+                    },
+            {  "title" :  "What does HTML stand for?",
+                "type"  :  "select",
+                "option" : [
+                    { 
+                        "choice" : "option A"
+                        "choice" : "option B"
+                        "choice" : "option C"
+                        "choice" : "option D"
+                    },
+            {  
+                "title" :  "What does HTML stand for?",
+                "type"  :  "select",
+                "option" : [
+                    { 
+                        "choice" : "option A"
+                        "choice" : "option B"
+                        "choice" : "option C"
+                        "choice" : "option D"
+                    },
+            {  
+                "title" :  "What does HTML stand for?",
+                "type"  :  "select",
+                "option" : [
+                    { 
+                        "choice" : "option A"
+                        "choice" : "option B"
+                        "choice" : "option C"
+                        "choice" : "option D"
+                    },
+            {  
+                "title" :  "What does HTML stand for?",
+                "type"  :  "select",
+                "option" : [
+                    { 
+                        "choice" : "option A"
+                        "choice" : "option B"
+                        "choice" : "option C"
+                        "choice" : "option D"
+            
+                ]
+          
+    }
