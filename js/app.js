62deb34be2050f507f8c3cdced3528875967215b
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2019-11-10 16:37:08 +0100

    =all pages connected and meter animation

--- js/app.js
+++ js/app.js
@@ -6,10 +6,11 @@
         option : 
         [
             { 
-                    "choice1" : "HyperText Markup Language",
+                    "choice1" : "HyperText Markup Language", 
                     "choice2" : "HyperLink Markup Language",
                     "choice3" : "HyperTransfer Markup Language",
-                    "choice4" : "HyperTextual Markup Languages"
+                    "choice4" : "HyperTextual Markup Languages",
+                    "answer" : "HyperText Markup Language"                  
             }, 
         ]
     },
@@ -100,48 +101,104 @@
 ]
 
 var i=0; 
-function mostrar(index){
 
-document.getElementById("question").innerHTML = questions[index].title;
-document.getElementById("choice1").innerHTML = questions[index].option[0].choice1;
-document.getElementById("choice2").innerHTML = questions[index].option[0].choice2;
-document.getElementById("choice3").innerHTML = questions[index].option[0].choice3;
-document.getElementById("choice4").innerHTML = questions[index].option[0].choice4;
+var nextButton = document.getElementById( "nextButton" );
+function mostrar(i){
 
+document.getElementById("question").innerHTML = questions[i].title;
+document.getElementById("choice1").innerHTML = questions[i].option[0].choice1;
+document.getElementById("choice2").innerHTML = questions[i].option[0].choice2;
+document.getElementById("choice3").innerHTML = questions[i].option[0].choice3;
+document.getElementById("choice4").innerHTML = questions[i].option[0].choice4;
+
+
+if(window.location.pathname == "/C:/Users/alfon/Desktop/final-quiz/final-quiz/html/htmlquestions.html") {
+    switch(i) {
+        case 6:
+        window.location.href="./cssquestions.html"
+        break;
+        default:
+          // Well 
+    }  
 }
 
+if(window.location.pathname == "/C:/Users/alfon/Desktop/final-quiz/final-quiz/html/cssquestions.html") {
+    switch(i) {
+        case 6:
+        window.location.href="./javascriptquestions.html"
+        break;
+        default:
+          // Well 
+    }  
+}
+if(window.location.pathname == "/C:/Users/alfon/Desktop/final-quiz/final-quiz/html/javascriptquestions.html") {
+    switch(i) {
+        case 5:
+        window.location.href="./ranking.html"
+        break;
+        default:
+          // Well 
+    }  
+}
+
+
+// if(i == 6) {
+//     i = 0
+//     window.location.href="./cssquestions.html"
+// }
+
+}
 mostrar(i);
+
+// function checkAnswer(){
+//     const label = document.querySelectorAll(".label");
+//     const radio = document.querySelectorAll(".a");
+//     let count;
+// //     for(i=0; i < 4; i++) {
+// //         console.log(radio[i].checked)
+// //     if(radio[i].checked == true) {
+// //         console.log(questions[i].option[0].answer)
+// //         console.log(label[i].value);
+// //     if(label[i].value == questions[i].option[0].answer) {
+// //         console.log(count);
+// //     count++;
+// //     console.log(count);
+// //     }
+// //     }
+// //     }
+
+// // }
+
 
 // localStorage.setItem( "person", JSON.stringify ( questions ) );
 var nextButton = document.getElementById( "nextButton" );
-nextButton.onclick = function(){
+nextButton.onclick = function (){
+    // checkAnswer();
     i++;
-    console.log(i);
     mostrar(i);
+    // checkAnswer();
+
+var max = parseInt(meterHTML.getAttribute("max"));
+var value = parseInt(meterHTML.getAttribute("value"));
+
+    if ((value<max)) {
+        value += 15;
+        document.getElementById("meterHTML").value = value;
+    }
 }
 
-// var meterHTML = document.getElementById("meterHTML");
-// var max = parseInt(meterHTML.getAttribute("max"));
-// var value = parseInt(meterHTML.getAttribute("value"));
-// var next = document.getElementById("nextButton");
-// next.onclick = function(){
-//     if ((value<max)) {
-//         value += 10;
-//         document.getElementById("meterHTML").value = value;
-//         console.log(value);
-//     } 
-// }
 
 
-// function checked(){
-//     for (let index = 0; index < arrayInput.length; index++) {
-//         if (arrayInput[index].checked == true){
-//             console.log(arrayInput[index].value);
-//         }
-//     }
-// }
+function checked(){
+    let inputsArray = document.querySelectorAll(".a")
+    for (let index = 0; index < 4; index++) {
+        if (inputsArray[index].checked == true){
+            console.log(inputsArray[index].checked.value);
+        }
+    }
+}
 
-// checked();
+
 
 
 function saveName(){
@@ -154,11 +211,4 @@ function saveName(){
 function showName(){
     document.getElementById("userNameGeneral").innerHTML = localStorage.getItem("username");
 }
-
-function checkAnswer(){
-    // let rightanswer = document.getElementById("choice1");
-}
-
-
-
 
